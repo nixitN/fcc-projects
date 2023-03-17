89bfdfeed345844a2640163d9dec1010d4d1ac38
Nikolett Toth <nixitn@gmail.com>
2023-03-17 14:57:38 +0100

    Changed variable names

--- python/arithmetic-formatter/arithmetic_arranger.py
+++ python/arithmetic-formatter/arithmetic_arranger.py
@@ -4,10 +4,10 @@ def arithmetic_arranger(problems, solve = False):
     if(len(problems) > 5):
         return "Error: Too many problems."
     
-    first = ""
-    second = ""
+    first_row = ""
+    second_row = ""
     lines = ""
-    sumx = ""
+    summary = ""
     string = ""
 
     for problem in problems:
@@ -16,40 +16,40 @@ def arithmetic_arranger(problems, solve = False):
                 return "Error: Operator must be '+' or '-'."
             return "Error: Numbers must only contain digits."
         
-        firstNumber = problem.split(" ")[0]
+        first_number = problem.split(" ")[0]
         operator = problem.split(" ")[1]
-        secondNumber = problem.split(" ")[2]
+        second_number = problem.split(" ")[2]
 
-        if(len(firstNumber) >= 5 or len(secondNumber) >= 5):
+        if(len(first_number) >= 5 or len(second_number) >= 5):
             return "Error: Numbers cannot be more than four digits."
         
         sum = ""
         if(operator == "+"):
-            sum = str(int(firstNumber) + int(secondNumber))
+            sum = str(int(first_number) + int(second_number))
         elif(operator == "-"):
-            sum = str(int(firstNumber) - int(secondNumber))
+            sum = str(int(first_number) - int(second_number))
 
-        length = max(len(firstNumber), len(secondNumber)) + 2
-        top = str(firstNumber).rjust(length)
-        bottom = operator + str(secondNumber).rjust(length - 1)
+        length = max(len(first_number), len(second_number)) + 2
+        top = str(first_number).rjust(length)
+        bottom = operator + str(second_number).rjust(length - 1)
         line = ""
-        res = str(sum).rjust(length)
+        value = str(sum).rjust(length)
         for s in range(length):
             line += "-"
 
         if problem != problems[-1]:
-            first += top + '    '
-            second += bottom + '    '
+            first_row += top + '    '
+            second_row += bottom + '    '
             lines += line + '    '
-            sumx += res + '    '
+            summary += value + '    '
         else:
-            first += top
-            second += bottom
+            first_row += top
+            second_row += bottom
             lines += line
-            sumx += res
+            summary += value
 
     if solve:
-        string = first + "\n" + second + "\n" + lines + "\n" + sumx
+        string = first_row + "\n" + second_row + "\n" + lines + "\n" + summary
     else:
-        string = first + "\n" + second + "\n" + lines
+        string = first_row + "\n" + second_row + "\n" + lines
     return string
